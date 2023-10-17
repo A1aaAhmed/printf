7c1cd6d51a06627e7efa45618ab170abb1c35e76
A1aaAhmed <alaaahmad20000@gmail.com>
2023-10-17 15:49:36 -0700

    tasks

--- _putchar.c
+++ _putchar.c
@@ -1,12 +1,28 @@
 #include "main.h"
 
 /**
- * _putchar - prints/writes character
- * @character: The character to print
- * Return: 1, success, -1 error
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-
-int _putchar(char character)
+int _putchar(char c)
 {
-	return (write(1, &character, 1));
+	return (write(1, &c, 1));
+}
+
+
+/**
+ * _puts - prints a string to stdout
+ * @str: pointer to the string to print
+ * Return: number of chars written
+ */
+int _puts(char *str)
+{
+	register short i;
+
+	for (i = 0; str[i]; i++)
+		_putchar(str[i]);
+	return (i);
 }

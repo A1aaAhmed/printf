d3196feba019b043025d12d5d62ca90540822b0c
A1aaAhmed <alaaahmad20000@gmail.com>
2023-10-16 18:24:15 -0700

    tasks

--- main.h
+++ main.h
@@ -1,3 +1,6 @@
+ifndef MAIN_H
+#define MAIN_H
+
 #include <stdarg.h>
 
 int _putchar(char c);
@@ -18,3 +21,5 @@ int print_base16_upper_lower(va_list arg, char *representation);
 int loop_format(va_list arg, const char *string);
 int call_function_manager(char aux, va_list arg);
 int check_percent(int *flag, char aux);
+
+#endif
